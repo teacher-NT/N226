bb09a03bf6f04e4dcb2383372776cf441550f2cc
Teacher NT <alpholmon@gmail.com>
2026-09-18 10:35:40 +0500

    tuple1

--- tuple1.py
+++ tuple1.py
@@ -26,18 +26,39 @@ mevalar = ('olma', 'banan', 'gilos', 'anor', 'kivi')
 # ===================================================================
 
 
-mevalar = ('olma', 'banan', 'gilos', 'anor', 'kivi')
+# mevalar = ('olma', 'banan', 'gilos', 'anor', 'kivi')
 
-print(mevalar[2])
+# print(mevalar[2])
 
-print(mevalar[2:5])
+# print(mevalar[2:5])
 
-print(mevalar[1:4:2])
+# print(mevalar[1:4:2])
 
-print(mevalar[3:])
+# print(mevalar[3:])
 
-print(mevalar[:3])
+# print(mevalar[:3])
 
-print(mevalar[-2])
+# print(mevalar[-2])
 
-print(mevalar[::-1])
+# print(mevalar[::-1])
+
+# ===================================================================
+
+# tuple1 = (1,2,3,4)
+# tuple2 = ('a', 'b', 'c')
+# tuple3 = tuple1 + tuple2
+
+# print(tuple3)
+
+mevalar = ('olma', 'anor', 'banan')
+# a = mevalar[0]
+# b = mevalar[1]
+# c = mevalar[2]
+
+# a,b,c = mevalar
+# print(a,b,c)
+
+
+# sonlar = (1,2,3,4,5,6,7,8)
+# a,*b,c = sonlar
+# print(a,b,c)
