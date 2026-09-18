5d5a199dc26a0be001a02e57a63e611878ac09dc
Teacher NT <alpholmon@gmail.com>
2026-09-18 10:16:42 +0500

    tuple1

--- main.py
+++
@@ -1,9 +0,0 @@
-import os
-os.system("cls")
-
-mytuple1 = (1,2,3,"Salom", 'Xayr', 'Korishguncha')
-
-mytuple1[3] = "Hello"
-
-print(mytuple1)
-print(mytuple1[3])
---
+++ tuple1.py
@@ -0,0 +1,24 @@
+import os
+os.system("cls")
+
+# mytuple1 = (1,2,3,"Salom", 'Xayr', 'Korishguncha')
+
+# mytuple1[3] = "Hello"
+
+# print(mytuple1)
+# print(mytuple1[3])
+
+
+
+mevalar = ('olma', 'banan', 'gilos', 'anor', 'kivi')
+# if 'gilos' in mevalar:
+#     print("Bor")
+# else:
+#     print("Yo'q")
+
+
+# for i in range(5):
+#     print(i, mevalar[i])
+
+for i in mevalar:
+    print(i)
