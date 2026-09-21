25d815b7eb017cb7d6adb47934a9e6f39ce092d5
Teacher NT <alpholmon@gmail.com>
2026-09-21 09:47:51 +0500

    set

---
+++ Non-primitive/set1.py
@@ -0,0 +1,18 @@
+import os
+os.system("cls")
+
+set1 = {19,18,16,35,12,18,12}
+# set1[2] = 7
+# print(set1[2])
+
+# print(set1)
+
+set2 = {'olma', 'anor', 'banan', 'gilos', 'shaftoli'}
+
+# if 'banan' in set2:
+#     print("Yes")
+# else:
+#     print("No")
+
+# for i in set2:
+#     print(i, end=" ")
