48dc419a2409243c3c55376d9c9d7bbd27818b76
Teacher NT <alpholmon@gmail.com>
2026-09-21 10:08:32 +0500

    set

--- Non-primitive/set1.py
+++ Non-primitive/set1.py
@@ -16,3 +16,38 @@ set2 = {'olma', 'anor', 'banan', 'gilos', 'shaftoli'}
 
 # for i in set2:
 #     print(i, end=" ")
+
+
+# =======================================
+
+set3 = {'Damas', 'Gentra', 'Nexia', 'Spark', 'Chempion'}
+
+# set3.add('BMW')
+# print(set3)
+
+# set3.remove('Spark')
+# print(set3)
+
+# set3.discard("Matiz")
+# print(set3)
+
+# m = set3.pop()
+# print(set3)
+# print(m)
+
+# set3.clear()
+# print(set3)
+
+# set4 = set3.copy()
+# set4.add("Buggati")
+# print(set3)
+# print(set4)
+
+set4 = {'Damas', "Captiva", 'Cobalt', 'Tracker', 'Malibu'}
+
+# set3.update(set4)
+# print(set3)
+
+# set5 = set3.union(set4)
+# print(set5)
+# print(set3)
