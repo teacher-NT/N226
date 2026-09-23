557abcc0fee2d8512974c29f7db3d9df3d7c1f19
Teacher NT <alpholmon@gmail.com>
2026-09-23 09:46:49 +0500

    Dict

--- Non-primitive/dict1.py
+++ Non-primitive/dict1.py
@@ -20,5 +20,38 @@ car = {
 # else:
 #     print("No")
 
-for i in car:
-    print(i, car[i])
+# for i in car:
+#     print(i, car[i])
+
+# print(car['rangi'])
+# print(car.get('rangi', "Bunday kalit yo'q"))
+
+# kalitlar = car.keys()
+# print(kalitlar)
+
+# qiymatlar = car.values()
+# print(qiymatlar)
+# if "Matiz" in qiymatlar:
+#     print("Bor")
+# else:
+#     print("Yo'q")
+
+# k = car.pop("model")
+# print(car)
+# print(k)
+
+# k = car.popitem()
+# print(car)
+# print(k)
+
+# print(car.items())
+# for i in car:
+#     print(i, car[i])
+# for k, q in car.items():
+#     print(k, q)
+
+# car['model'] = "Spark"
+# car["narx"] = 1500
+# car["rang"] = "Yashil"
+car.update({"model": "Spark", "narx":1500, 'rang':"Yashil"})
+print(car)
